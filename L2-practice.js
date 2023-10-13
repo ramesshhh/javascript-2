d5549fd69734d24be31915ce3ecafe188517de70
ramesshhh <rameshjha55061@gmail.com>
2023-10-13 16:22:41 +0530

    Datatypes

--- L2-practice.js
+++ L2-practice.js
@@ -2,3 +2,12 @@ const accountId = 1234;
 let accountEmail = "Ramesh@mail.com";
 var accountPassword = "qwerty";
  accountCity = "Bokaro Steel City";
+
+
+console.log();
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ L3-DataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all JS code as newer version
+
+let age = 20;
+
+// alert(3+3);   // we are using node js not browser
+
+/*
+    //  types of data types is javascript
+    number => 2 to power 53
+    bigint => very big value
+    string => " "
+    boolean => true or false
+    null => it is a stand alone value => representation of empty value
+    undefined => variable is defined but value is not given
+    symbole => for unique value
+    object => 
+*/
+
+console.log(typeof age);
+
+console.log(typeof null);     // gives value object
+console.log(typeof undefined);   // gives value undefined
